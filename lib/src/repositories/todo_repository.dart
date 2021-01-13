363c55f60c7c150e7b7666da78fa782fa067564b
Samuel Matias <hi@samuelematias.com>
2021-01-13 09:48:39 -0300

    refactor(repository): improve todo_repository code

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -5,24 +5,11 @@ class TodoRepository {
   Dio dio;
   final String url = 'https://jsonplaceholder.typicode.com/todos';
 
-  TodoRepository([Dio client]) {
-    if (client == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
-  }
+  TodoRepository([Dio client]) : dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
     final Response response = await dio.get(url);
     final List list = response.data as List;
-    List<TodoModel> todos = [];
-
-    for (var json in list) {
-      final TodoModel todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
