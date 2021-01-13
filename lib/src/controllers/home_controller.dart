10a4abd548f882fd43cf462145f6848a025ad851
Samuel Matias <hi@samuelematias.com>
2021-01-13 13:59:07 -0300

    refactor(controller): implement ValueNotifier

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,21 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:list_todos/src/models/todo_model.dart';
 import 'package:list_todos/src/repositories/todo_repository.dart';
 
 class HomeController {
   List<TodoModel> todos = [];
   final TodoRepository _repository;
-  HomeState state;
+  final ValueNotifier<HomeState> state =
+      ValueNotifier<HomeState>(HomeState.loading);
 
   HomeController([TodoRepository repository])
       : _repository = repository ?? TodoRepository();
 
   Future start() async {
     try {
-      state = HomeState.loading;
+      state.value = HomeState.loading;
       todos = await _repository.fetchTodos();
-      state = HomeState.success;
+      state.value = HomeState.success;
     } catch (e) {
-      state = HomeState.error;
+      state.value = HomeState.error;
     }
   }
 }
